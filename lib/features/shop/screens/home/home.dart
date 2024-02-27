f29fc97e1d755b25c59d6b660e018a4080e67faf
Arizal Nur Rohman <arizalnurrohman13@gmail.com>
2024-02-27 15:47:19 +0700

    next

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
 import 'package:gurindam/features/shop/screens/home/widgets/home_appbar.dart';
+// import 'package:gurindam/utils/constants/colors.dart';
+import 'package:gurindam/utils/constants/image_strings.dart';
+// import 'package:gurindam/utils/helpers/helper_functions.dart';
 // import 'package:gurindam/utils/constants/colors.dart';
 // import 'package:gurindam/utils/devices/device_utility.dart';
 // import 'package:gurindam/utils/helpers/helper_functions.dart';
@@ -10,6 +14,7 @@ import 'package:gurindam/features/shop/screens/home/widgets/home_appbar.dart';
 //import '../../../../common/widgets/appbar/appbar.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
 import '../../../../utils/constants/sizes.dart';
 
@@ -22,26 +27,50 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
               child: Column(
                 children: [
-                  THomeAppBar(),
-                  SizedBox(height: TSizes.spaceBtwSections),
-                  TSearchContainer(text: 'Cari Materi Pembelajaran'),
-                  SizedBox(height: TSizes.spaceBtwSections),
+                  ///-- Appbar
+                  const THomeAppBar(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  ///-- seach box
+                  const TSearchContainer(text: 'Cari Materi Pembelajaran'),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  ///-- categories
                   Padding(
                     padding: const EdgeInsets.only(left: TSizes.defaultSpace),
                     child: Column(
                       children: [
-                        TSelectionHeading(
+                        ///-- heading
+                        const TSelectionHeading(
                           title: 'Popular Categories',
                           showActionButton: false,
+                          textColor: Colors.white,
                         ),
-                        SizedBox(height: TSizes.spaceBtwItems),
+                        const SizedBox(height: TSizes.spaceBtwItems),
+
+                        ///-- Categories
+                        SizedBox(
+                          height: 80,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (_, index) {
+                              return TVerticalImageText(
+                                image: TImages.shoeIcon,
+                                title: 'Shoes',
+                                onTap: () {},
+                              );
+                            },
+                          ),
+                        ),
                       ],
                     ),
                   ),
